a3fd30bb25e98c0c691caa405c103a73dde30f65
Tien <ngotienpts@gmail.com>
2023-06-23 17:23:47 +0700

    23/6

---
+++ components/MovieLList.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const MovieLList = () => {
+    return (
+        <View>
+            <Text>MovieLList</Text>
+        </View>
+    );
+};
+
+export default MovieLList;
---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,64 @@
+import {
+    View,
+    Text,
+    Platform,
+    TouchableOpacity,
+    ScrollView,
+} from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import {
+    Bars3CenterLeftIcon,
+    MagnifyingGlassIcon,
+} from "react-native-heroicons/outline";
+
+import { styles } from "../theme";
+import TrendingMovies from "../components/TrendingMovies";
+import MovieLList from "../components/MovieLList";
+
+const ios = Platform.OS == "ios";
+const HomeScreen = () => {
+    const [trending, setTrending] = useState([1, 2, 3]);
+    const [upcoming, setUpcoming] = useState([1, 2, 3]);
+    const [topRated, setTopRated] = useState([1, 2, 3]);
+    return (
+        <View className="flex-1 bg-neutral-800">
+            {/* search bar and logo */}
+            <SafeAreaView className={ios ? "-mb-2" : "my-3"}>
+                <StatusBar style="light" />
+                <View className="flex-row items-center justify-between mx-4">
+                    <Bars3CenterLeftIcon
+                        size={30}
+                        strokeWidth={2}
+                        color={"white"}
+                    />
+                    <Text className="text-white font-bold text-3xl">
+                        <Text style={styles.text}>M</Text>ovies
+                    </Text>
+                    <TouchableOpacity>
+                        <MagnifyingGlassIcon
+                            size={30}
+                            strokeWidth={2}
+                            color={"white"}
+                        />
+                    </TouchableOpacity>
+                </View>
+            </SafeAreaView>
+
+            {/* body */}
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ paddingBottom: 10 }}
+            >
+                {/* trending movies carousel */}
+                <TrendingMovies data={trending} />
+
+                {/* upcoming */}
+                <MovieLList title="Upcoming" data={upcoming} />
+            </ScrollView>
+        </View>
+    );
+};
+
+export default HomeScreen;
